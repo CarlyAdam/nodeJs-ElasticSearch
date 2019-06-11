cfbd0d43d72e007c579b3a115eb6e5747df76ec4
Adam <carlyadam88@gmail.com>
2019-06-11 11:19:23 -0500

    get operation created

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -3,3 +3,18 @@ const db = require('../../config/db/');
 const Student = require('../models/student');
 
 const client = new Client({ node: process.env.ES_URL });
+
+// Get all students
+exports.getStudents = async (req, res) => {
+  try {
+      const students = await Student.find();        
+       return  res.json({
+        students,
+  });
+       
+  } catch (err) {
+     return  res.status(500).send('Something broke!');
+    
+   
+  }
+};

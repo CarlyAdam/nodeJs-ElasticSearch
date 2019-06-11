0a2212189ef756ef8949b84c0bd772a4e1a73374
Adam <carlyadam88@gmail.com>
2019-06-11 11:17:01 -0500

    model created

---
+++ src/api/models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Student', schema);
